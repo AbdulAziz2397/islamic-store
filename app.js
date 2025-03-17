6e1c24664d404481aee00e1f45c1e5b706369d28
AbdulAziz2397 <abdulaziztahir2397@gmail.com>
2025-03-17 10:00:28 +0500

    islamic store

--- app.js
+++ app.js
@@ -1,242 +1,32 @@
-// var x = 5;
-// var b = x++ + ++x + x + --x + x-- - x;
-// alert(b);
+let nextBtn = document.querySelector(".next");
+let prevBtn = document.querySelector(".prev");
+let slider = document.querySelector(".slider");
+let sliderList = document.querySelector(".slider .list");
+let thumbnail = document.querySelector(".slider .thumbnail");
+let thumbnailItems = document.querySelectorAll(".item");
+thumbnail.appendChild(thumbnailItems[0]);
+nextBtn.onclick = function () {
+    moveSlider("next");
+};
+prevBtn.onclick = function () {
+    moveSlider("prev");
+};
+function moveSlider(direction) {
+    let sliderItems = sliderList.querySelectorAll(".item");
+    let thumbnailItems = document.querySelectorAll(".thumbnail .item")
 
-// var a = +prompt("Enter your age","your age is:")
-// var b = a + 10;
-// document.write("The output is"," ",b);
-
-// var age = +prompt("Enter Your age:");
-// if (age == 14){
-//     alert("your age is equal")
-// }
-// else if(age < 14){
-//     alert("Your age is else")
-// }
-// else{
-//     alert("Your age is more")
-// }
-
-// var x = 5;
-// var y = x++ + ++x + x-- + --x - x + x;
-// alert(y)
-
-// var age = document.write+prompt("Enter Your Age");
-// var gender
-// if (age >= 18){
-//     gender = prompt("Enter Your Gender");
-//     if(gender == 'male'&& age >= 18){
-//         document.write("You are allowed to ride")
-//     }else{
-//         document.write("You are not allowed to ride")
-//     }
-// }
-// else{
-//     document.write("You are not allowed to ride")
-// }
-
-// var age = +prompt("Enter your age");
-// var gender;
-// if (age >= 18){
-//     gender = prompt("Enter your gender")
-//     if(gender === "male" && age >= 18){
-//         alert("You are allowed to ride")
-//     }
-//     else{
-//         alert("You are not allowed to ride")
-// }
-// }
-// else{alert
-//     ("You are not allowed to ride")
-// }
-
-// var age = +prompt("Enter your age")
-// var gender
-// if (age == 18 || age == 19 || age == 20 ){
-//     gender = prompt("Enter your gender")
-//     if (gender === "male" && age >= 18){
-//         alert("you are allowed to ride")
-//     }
-//     else{
-//         alert("you are not allowed to ride")
-//     }
-// }
-// else{
-//     alert("you are not allowed to ride")
-// }
-
-// var a = 20;
-// var b = "10";
-// var c = (a/10)
-// alert(c)
-
-// var age = +prompt("Enter your age");
-// var gender;
-// var contry;
-// var weight;
-// var height;
-// if (age >= 18 && age <= 100) {
-//     gender = prompt("Enter your gender")
-//     if (gender === "male") {
-//         contry = prompt("Enter your contry name");
-//         if (contry !== "israeel" && contry !== "U.S.A" && contry !== "usa" && contry !== "india") {
-//             weight = prompt("Enter your weight")
-//             if (weight <= 110 && weight >= 30) {
-//                 height = +prompt("Enter your height  in feets")
-//                 if (height >= 6) {
-//                     alert("You are able to adding")
-//                 }
-//                 else {
-//                     alert("You are unable to adding")
-//                 }
-//             }
-//             else {
-//                 alert("You are unable to adding")
-//             }
-//         }
-//         else {
-//             alert("You are unable to adding")
-//         }
-//     }
-//     else {
-//         alert("You are unable to adding")
-//     }
-// }
-// else {
-//     alert("You are unable to adding")
-// }
-
-// var percent = +prompt("Enter your percent")
-// if (percent <=100 && percent >=80){
-//     alert("A+")
-// }
-// else if(percent <80 && percent >=70){
-//     alert("A")
-// }
-// else if(percent <70 && percent >=60){
-//     alert("B")
-// }
-// else if(percent <60 && percent >=50){
-//     alert("C")
-// }
-// else if(percent <50 && percent >=40){
-//     alert("D")
-// }
-// else if(percent <40 && percent >=33){
-//     alert("E")
-// }
-// else if(percent < 33 && percent >0){
-//     alert("F")
-// }
-// else if(percent === 0){
-//     alert("F")
-// }
-// else{
-//     alert("You did not write correct percentage")
-// }
-
-// var a = "ali";
-// var b = "ghous";
-// var c = "basit";
-
-// var gamers = ["affad" , "farooq" , "sami" , "asad" , "khalil" , "yaqoob" , "anas" , "hamzah"]
-// var friends
-// gamers.pop()
-// gamers.push("usman")
-// gamers.shift()
-// gamers.unshift("mehmood")
-// gamers.splice(1,2,"muhammad")
-// friends = gamers.slice(2,4)
-// alert(gamers)
-// alert(friends)
+    if (direction === "next") {
+        sliderList.appendChild(sliderItems[0]);
+        thumbnail.appendChild(thumbnailItems[0]);
+        slider.classList.add("next");
+    } else {
+        sliderList.prepend(sliderItems[sliderItems.length - 1]);
+        thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1]);
+        slider.classList.add("prev");
+    }
 
 
-// var a = 12;
-// var b = 15;
-// alert( a + "username" + b)
-
-
-// var age = prompt("enter your age" , "your age is")
-
-// var age = "enter your  age";
-// var ageis = "your age is";
-// var a = prompt(age , ageis);
-// alert(a)
-
-// var numberOfCats = prompt("How many cats?");
-// var tooManyCats = numberOfCats +1;
-// alert(tooManyCats)
-
-// var a = prompt("are these days vications?")
-// if (a === "yes"){ 
-//     alert("correct")
-// }
-// else{
-//     alert("incorrect")
-// }
-
-// var x = +prompt("enter your age")
-// var age = 15;
-// if (x === age){
-//     alert("correct")
-// }
-// else{
-//     alert("incorrect")
-// }
-
-// var age = 15;
-// var userIQ;
-// var score = 0;
-// var a = +prompt("enter yor age")
-// if (a === age){
-//     userIQ = "best";
-//     score++
-//     alert("correct")
-// }
-// document.write("score:" + score);
-// document.write(userIQ);
-
-// var firstname = prompt("enter you firstname");
-// var secondname;
-// var age;
-// var contryname;
-// var gender;
-// var phonenumber;
-
-
-// if(firstname !== "") {
-//     secondname = prompt("enter you secondname")
-//     if(secondname !== "") {
-//         age = +prompt("enter your age")
-//         if(age <= 90 && age >= 18) {
-//             contryname = prompt("enter you contry name")
-//             if(contryname !=="israel" && contryname !== "") {
-//                 gender = prompt ("enter your gender")
-//                 if( gender || "male" && gender || "female" && gander !== ""){
-//                     phonenumber = +prompt("enter your phone number")
-//                     if(phonenumber !== ""){
-//                         alert("Your attempt is successful.")
-//                     }
-//                     else{
-//                         alert("your attempt is not successful.")
-//                     }
-//                 }
-//                 else{
-//                     alert("your attempt is not successful.")
-//                 }
-//             }
-//             else{
-//                 alert("your attempt is not successful.")
-//             }
-//         }
-//         else{
-//             alert("your attempt is not successful.")
-//         }
-//     }
-//     else{
-//         alert("your attempt is not successful.")
-//     }
-// }
-// else{
-//     alert("your attempt is not successful.")
-// }
+    slider.addEventListener( 
+        "animationed",
+    )
+}

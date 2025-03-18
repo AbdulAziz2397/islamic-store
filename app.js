22866a953a680dae34d1014bd7544b02e3ca26b3
AbdulAziz2397 <abdulaziztahir2397@gmail.com>
2025-03-18 07:01:08 +0500

    islamic store

--- app.js
+++ app.js
@@ -12,26 +12,26 @@ thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
 let timeRunning = 3000;
 let timeAutoNext = 3800;
 
-nextDom.onclick = function(){
-    showSlider('next');    
+nextDom.onclick = function () {
+    showSlider('next');
 }
 
-prevDom.onclick = function(){
-    showSlider('prev');    
+prevDom.onclick = function () {
+    showSlider('prev');
 }
 let runTimeOut;
 let runNextAuto = setTimeout(() => {
     next.click();
 }, timeAutoNext)
-function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+function showSlider(type) {
+    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
-    if(type === 'next'){
+
+    if (type === 'next') {
         SliderDom.appendChild(SliderItemsDom[0]);
         thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
         carouselDom.classList.add('next');
-    }else{
+    } else {
         SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
         thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
         carouselDom.classList.add('prev');
@@ -97,47 +97,50 @@ function showSlider(type){
 // });
 
 // ............. collection slider .................
-
 const collectionSlider = document.querySelector(".collectionSlider");
 const form = document.querySelector(".form");
 let mouseDownAt = 0;
 let left = 0;
-collectionSlider.onmousedown = (e) => {
-    mouseDownAt = e.clientX;
-    console.log(mouseDownAt);
-};
-collectionSlider.onmouseup = () => {
-    mouseDownAt = 0;  
+
+function handleMove(clientX) {
+    if (mouseDownAt == 0) return;
+    collectionSlider.style.userSelect = 'none';
+    collectionSlider.style.cursor = 'grab';
+    form.style.pointerEvents = 'none';
+
+    if (clientX > mouseDownAt) {
+        form.classList.add('left');
+        form.classList.remove('right');
+    } else if (clientX < mouseDownAt) {
+        form.classList.remove('left');
+        form.classList.add('right');
+    }
+
+    let speed = 3;
+    let leftTemporary = left + ((clientX - mouseDownAt) / speed);
+    let leftLimit = form.offsetWidth - collectionSlider.offsetWidth / 2;
+
+    if (leftTemporary < 0 && Math.abs(leftTemporary) < leftLimit) {
+        form.style.setProperty('--left', left + 'px');
+        left = leftTemporary;
+        mouseDownAt = clientX;
+    }
+}
+
+function resetSlider() {
+    mouseDownAt = 0;
     collectionSlider.style.userSelect = 'unset';
     collectionSlider.style.cursor = 'unset';
     form.style.pointerEvents = 'unset';
     form.classList.remove('left');
     form.classList.remove('right');
 }
-collectionSlider.onmousemove = e => {
-    if(mouseDownAt == 0) return;
-    collectionSlider.style.userSelect = 'none';
-    collectionSlider.style.cursor = 'grab';
-    form.style.pointerEvents = 'none';
-    
-    if(e.clientX > mouseDownAt){
-        form.classList.add('left');
-        form.classList.remove('right');
-    }else if(e.clientX < mouseDownAt){
-        form.classList.remove('left');
-        form.classList.add('right');
-    }
-    // increase or decrease the speed 
-    //by changing the value of 'speed'
-    let speed = 3;
-    let leftTemporary = left + ((e.clientX - mouseDownAt) / speed);
-    let leftLimit = form.offsetWidth - collectionSlider.offsetWidth / 2;
 
-    
-    if(leftTemporary < 0 && Math.abs(leftTemporary) < leftLimit){
-        form.style.setProperty('--left', left + 'px');
-        left = leftTemporary;
-        mouseDownAt = e.clientX;
-    }
+collectionSlider.onmousedown = (e) => mouseDownAt = e.clientX;
+collectionSlider.onmousemove = (e) => handleMove(e.clientX);
+collectionSlider.onmouseup = resetSlider;
 
-}
+// Touch Events
+collectionSlider.addEventListener('touchstart', (e) => mouseDownAt = e.touches[0].clientX);
+collectionSlider.addEventListener('touchmove', (e) => handleMove(e.touches[0].clientX));
+collectionSlider.addEventListener('touchend', resetSlider);
